46b6abfef5b3df434b51599bf23c46558f9f894f
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-07-15 23:37:44 -0300

    refactoring

--- app/src/main/java/com/arcieri/wagner/mvvm_recipebook/ui/screen/widgets/RecipeImageAndInfo.kt
+++ app/src/main/java/com/arcieri/wagner/mvvm_recipebook/ui/screen/widgets/RecipeImageAndInfo.kt
@@ -1,8 +1,6 @@
 package com.arcieri.wagner.mvvm_recipebook.ui.screen.widgets
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -10,10 +8,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.arcieri.wagner.mvvm_recipebook.R
 import com.arcieri.wagner.mvvm_recipebook.model.Recipe
@@ -45,60 +43,23 @@ fun RecipeImageAndInfo(recipe: Recipe) {
         )
     }
 
-    Column(
-
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
+    GradientColumn(
         modifier = Modifier
-            .fillMaxSize(1f)
-
+            .fillMaxSize(),
+        columnHorizontalAlignment = Alignment.CenterHorizontally,
+        columnVerticalArrangement = Arrangement.Bottom ,
+        topSpacerTransition = true,
+        topSpacerTransitionHeight = 100.dp,
+        topTransitionTopColor = Color(0x00000000),
+        topTransitionMiddleColor = Color(0x1A000000),
+        topTransitionBottomColor = Color(0x4D000000),
+        bottomSpacerTransition = false,
+        columnTopColor = Color(0x4D000000),
+        columnMiddleColor = Color(0x4D000000),
+        columnBottomColor = Color(0xCC000000),
+        contentHorizontalAlignment = Alignment.CenterHorizontally,
+        contentVerticalArrangement =  Arrangement.SpaceBetween,
     ) {
-        Row(
-            modifier = Modifier
-                .weight(4f)
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        listOf(
-                            Color(0x00000000),
-                            Color(0x1A000000)
-                        ),
-                    )
-                )
-        ) { }
-
-        // GRADIENT TRANSITION ROW
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        listOf(
-                            Color(0x1A000000),
-                            Color(0x4D000000)
-                        ),
-                    )
-                )
-        ) { }
-
-
-        /** RECIPE NAME AND INFO */
-        Row(
-            modifier = Modifier
-                .weight(2f)
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        listOf(
-                            Color(0x4D000000),
-                            Color(0xCC000000)
-                        ),
-                    )
-                ),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            RecipeNameTimeRow(recipe, textColor = RB_White)
-        }
+        RecipeNameTimeRow(recipe, textColor = RB_White)
     }
 }
