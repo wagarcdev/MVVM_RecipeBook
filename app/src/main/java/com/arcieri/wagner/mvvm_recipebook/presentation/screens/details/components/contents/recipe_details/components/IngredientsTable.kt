b29517d7d50142c273d54d19a674044adf405e0e
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-08-04 16:50:25 -0300

    UI layout update

--- app/src/main/java/com/arcieri/wagner/mvvm_recipebook/presentation/screens/details/components/contents/recipe_details/components/IngredientsTable.kt
+++ app/src/main/java/com/arcieri/wagner/mvvm_recipebook/presentation/screens/details/components/contents/recipe_details/components/IngredientsTable.kt
@@ -1,29 +1,26 @@
 package com.arcieri.wagner.mvvm_recipebook.presentation.screens.details.components.contents.recipe_details
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.arcieri.wagner.mvvm_recipebook.model.Recipe
-import com.arcieri.wagner.mvvm_recipebook.presentation.widgets.IngredientItem
-import com.arcieri.wagner.mvvm_recipebook.presentation.widgets.IngredientsSectionTittleRow
 import com.arcieri.wagner.mvvm_recipebook.presentation.ui.theme.RB_Black
 import com.arcieri.wagner.mvvm_recipebook.presentation.ui.theme.RB_Gray
 import com.arcieri.wagner.mvvm_recipebook.presentation.ui.theme.RB_Transparent
 import com.arcieri.wagner.mvvm_recipebook.presentation.ui.theme.RB_White
+import com.arcieri.wagner.mvvm_recipebook.presentation.widgets.IngredientItem
+import com.arcieri.wagner.mvvm_recipebook.presentation.widgets.IngredientsSectionTittleRow
 
 @Composable
 fun IngredientsTable(recipe: Recipe) {
 
-    var isIngredientsVisible by remember { mutableStateOf(true) }
-
-    isIngredientsVisible = IngredientsSectionTittleRow(isIngredientsVisible)
+    IngredientsSectionTittleRow()
 
     Column(
         modifier = Modifier
@@ -38,25 +35,23 @@ fun IngredientsTable(recipe: Recipe) {
 
                 ingredient.adjustMeasuringUnits()
 
-                AnimatedVisibility(visible = isIngredientsVisible) {
-                    IngredientItem(
-                        ingredient,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Normal,
-                        fontColor =
-                            if ( index % 2 == 0 ) {
-                                RB_Black
-                            } else {
-                                RB_White
-                            },
-                        backgroundColor =
-                            if ( index % 2 == 0 ) {
-                                RB_Transparent
-                            } else {
-                                RB_Gray
-                            }
-                    )
-                }
+                IngredientItem(
+                    ingredient,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontColor =
+                    if ( index % 2 == 0 ) {
+                        RB_Black
+                    } else {
+                        RB_White
+                    },
+                    backgroundColor =
+                    if ( index % 2 == 0 ) {
+                        RB_Transparent
+                    } else {
+                        RB_Gray
+                    }
+                )
             }
         }//if
     }//Column
